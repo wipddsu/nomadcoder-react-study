4632a384ee271a9dac26283acd483b09a7eac7be
wipddsu <rlaalstn8158@naver.com>
2024-02-16 12:35:53 +0900

    nomadcoder-react-study

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [usd, setUsd] = useState('');
+  const [select, setSelect] = useState({});
+
+  useEffect(() => {
+    fetch('https://api.coinpaprika.com/v1/tickers')
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setSelect(json[0]);
+        setLoading(false);
+      });
+  }, []);
+
+  const handleChangeSelect = (e) => {
+    const seletedCoin = e.target.value;
+    const coinObj = coins.filter((coin) => coin.symbol === seletedCoin)[0];
+
+    setSelect(coinObj);
+    setUsd(0);
+  };
+
+  const onChange = (e) => {
+    setUsd(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Coins! {loading ? '' : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={handleChangeSelect} value={select.symbol}>
+          {coins.map((coin) => (
+            <option key={coin.id} value={coin.symbol}>
+              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      )}
+      <hr />
+      <div>
+        <input value={usd} type="number" onChange={onChange} disabled={loading ? true : false} placeholder="0" />
+        USD ={' '}
+        <input
+          value={loading ? '' : usd / select.quotes.USD.price}
+          type="number"
+          disabled={loading ? true : false}
+          placeholder="0"
+        />
+        {loading ? null : select.symbol}
+      </div>
     </div>
   );
 }
